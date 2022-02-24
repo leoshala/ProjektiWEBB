dc6f543b6870417c5e7c40ca7ca0bbaf87a05950
leoshala <leonardshala58@gmail.com>
2022-02-24 21:26:33 +0100

    Update home.php

--- LoginForma/home.php
+++ LoginForma/home.php
@@ -1,8 +1,14 @@
 <?php
+$hide="";
 session_start();
 if(!isset($_SESSION['email'])){
     header("location:LogIn.php");
 }else{
+    if($_SESSION['role'] == "admin" ){
+        $hide = "";
+    }else{
+        $hide = "hide";
+    }
 
 
 ?>
@@ -13,10 +19,16 @@ if(!isset($_SESSION['email'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .hide{
+            display:none;
+        }
+    </style>
 </head>
 <body>
     <h1>Home</h1>
     <a href="logout.php">Log out</a>
+    <a href="dashboard.php" class="<?php echo $hide ?>">Dashboard</a>
     <?php
     echo "Email: ".$_SESSION['email'];
     ?>
