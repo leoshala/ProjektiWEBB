8bf5e87ed91bdd10c4a704a74583b1935e079078
leoshala <leonardshala58@gmail.com>
2022-02-25 12:50:50 +0100

    Create registerController.php

---
+++ controller/registerController.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once '../repository/userRepository.php';
+include_once '../models/user.php';
+
+    if(isset($_POST['registerBtn'])){
+        if(empty($_POST['name']) || empty($_POST['surname']) || 
+        empty($_POST['username']) || empty($_POST['email']) || empty($_POST['password']) ||
+        empty($_POST['birthday']) || empty($_POST['tel'])){
+            echo "Fill all Fiellds!";
+        }else{
+            $name = $_POST['name'];
+            $surname = $_POST['surname'];
+            $username = $_POST['username'];
+            $email = $_POST['email']; 
+            $password = $_POST['password'];
+           // $birthday = $_POST['birthday'];
+          //  $tel = $_POST['tel'];
+            $id = $username.rand(100,999);
+
+        $user = new User($id,$name,$surname,$username,$email,$password,/*$birthday,$tel*/);
+            $userRepository = new UserRepository();
+
+            $userRepository->insertUser($user);
+        }
+    }
+
+?>
