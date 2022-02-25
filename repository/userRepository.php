55043063b78153e83732bcddc4005c5893883d6f
leoshala <leonardshala58@gmail.com>
2022-02-25 12:50:55 +0100

    Update userRepository.php

--- repository/userRepository.php
+++ repository/userRepository.php
@@ -10,23 +10,35 @@ include_once '../Database/databaseConnection.php';
         }
 
 
-
-        function insertUser(){
+                   // $user
+        function insertUser($user){
 
             $conn = $this->connection;
 
-            $sql = "INSERT INTO user (id,name,surname,email,username,password,birthday,tel) VALUES (?,?,?,?,?,?,?,?)";
+            $id = $user->getId();
+            $name = $user->getName();
+            $surname =$user->getSurname();
+            $username = $user->getUsername();
+            $email = $user->getEmail();
+            $password = $user->getPassword();
+            $birthday = $user->getBirthday();
+            $tel = $user->getTel();
+
+
+            $sql = "INSERT INTO user (id,name,surname,username,email,password,birthday,tel) VALUES (?,?,?,?,?,?,?,?)";
         
             $statement = $conn->prepare($sql);
 
-        $statement->execute(['1111','leo','shala','leoshala@gmail.com','Leoshala','Leo1234','2001/01/02','049123456']);
+     //   $statement->execute([$id,$name,$surname,$username,$email,$password,$birthday,$tel]);
+
+    // $statement->execute(['123','leo','shala','leoshala','leon@hasf.com','leo1234','2000/05/05','04912412']);
         
             echo "<script> alert('User u shtua me sukses');</script>";
         }
     }
 
-    $userRepo = new UserRepository;
+   // $userRepo = new UserRepository();
 
-    $userRepo->insertUser();
+   // $userRepo->insertUser();
 
 ?>
