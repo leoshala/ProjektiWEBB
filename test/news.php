f9c8bf641b3550b884326bc71fddf302e8b27094
pashtrikKrasniqi <100321406+pashtrikKrasniqi@users.noreply.github.com>
2022-02-27 21:40:57 +0100

    ora:9:40

--- test/news.php
+++ test/news.php
@@ -42,10 +42,10 @@ if(!empty($msg)){
         $db = mysqli_connect("localhost","root","","OLEKS");
         $sql = "SELECT * FROM images";
         $result = mysqli_query($db,$sql);
-        while($row = mysqli_fetch_array($result)){
-            echo "<div> id='img_div'>";
-            echo "<img src='img/".$row['image']."'>";
-            echo "<p>".$row['text']."</p>";
+        while($row = mysqli_fetch_array($result)) {
+            echo "<div id='img_div'>";
+            echo "<img src='img/".$row['img']."'>";
+            echo "<p>".$row['description']."</p>";
             echo "</div>";
         }
 
